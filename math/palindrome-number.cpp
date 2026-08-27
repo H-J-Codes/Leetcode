458b6dc1da1b5874733418917e8ce3a922c29a84
Hazel John <Coder.twenty.25@gmail.com>
2026-08-27 22:41:48 +0530

    Palindrome-number.cpp

---
+++ math/palindrome-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long n = 0;
+
+        while (x != 0) {
+            int dig = x % 10;
+            n = n * 10 + dig;
+            x /= 10;
+        }
+
+        if (n > INT_MAX || n < INT_MIN)
+            return 0;
+
+        return n;
+    }
+
+    bool isPalindrome(int x) {
+        if(x<0)return false;
+        if(x==reverse(x)){
+            return true;
+        }
+        return false;
+    }
+};
