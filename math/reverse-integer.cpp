41ecf7688c145af538abb98926e52b0695ea1f5a
Hazel John <Coder.twenty.25@gmail.com>
2026-08-27 22:38:30 +0530

    Reverse Integer

---
+++ math/reverse-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long n = 0;
+
+        while (x != 0) {
+            int dig = x % 10;
+            n = n * 10 + dig;
+            x /= 10;
+        }
+
+        if (n > INT_MAX || n < INT_MIN)
+            return 0;
+
+        return n;
+    }
+};
