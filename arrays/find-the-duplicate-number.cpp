76d73f43ce1d1ac6c17077946fa828752ed428e4
Hazel John <Coder.twenty.25@gmail.com>
2026-08-31 20:23:56 +0530

    LeetCode 287 (Floyd's Cycle Detection)

--- arrays/find-the-duplicate-number.cpp
+++ arrays/find-the-duplicate-number.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_set <int> s;
-        for(int val:nums){
-            if(s.find(val)!=s.end()){
-                return val;
-            }
-            s.insert(val);
+        int slow=nums[0],fast=nums[0];
+        do{
+            slow=nums[slow]; // +1
+            fast=nums[nums[fast]]; // +2
+        }while(slow!=fast);
+        slow=nums[0];
+        while(slow!=fast){
+            slow=nums[slow]; //+1
+            fast=nums[fast]; //+1
         }
-        return -1;
+        return slow; //or fast
     }
 };
